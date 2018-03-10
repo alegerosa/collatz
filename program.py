18cb074f7e65236ee599c473b749e82e0bf50fd8
Ale Gerosa <alegerosa@gmail.com>
2018-03-10 09:05:27 -0500

    Initial upload

---
+++ program.py
@@ -0,0 +1,15 @@
+number = int(raw_input("Number:"))
+count = 0
+orig_number= number
+while number !=1:
+    if number % 2 == 0:
+        number /=2
+        print number
+        count += 1
+    else:
+        number = number*3+1
+        print number
+        count += 1
+print "It took",count,"runs for",orig_number,"to go down to 1."
+
+# "My first python program, follows this http://www.someecards.com/life/school/collatz-conjecture/
